514657d809032c1157b4fbd3d53e8180a949a0a0
Daniel <danonienko8@gmail.com>
2025-05-30 10:32:55 +0300

    Unique

--- Lviv.TestAssignment.Database/Entities/Contact.cs
+++ Lviv.TestAssignment.Database/Entities/Contact.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.ObjectModel;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace Lviv.TestAssignment.Database.Entities
 {
+    [Index(nameof(Email), IsUnique = true)]
     public class Contact
     {
         public int Id { get; set; }
@@ -12,6 +14,7 @@ namespace Lviv.TestAssignment.Database.Entities
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
 
+        [Required]
         [EmailAddress]
         public required string Email { get; set; }
     }
